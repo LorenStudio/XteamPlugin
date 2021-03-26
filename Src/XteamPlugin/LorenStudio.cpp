65892abb5691fe9f724db7f89b7c3155d21b9a6c
Rodrigo <79727598+LorenStudio@users.noreply.github.com>
2021-03-26 10:40:24 -0300

    Removing multiple loading images

--- Src/XteamPlugin/LorenStudio.cpp
+++ Src/XteamPlugin/LorenStudio.cpp
@@ -10,6 +10,12 @@
 
 void Load()
 {
+	SetDword(0x004D57C7 + 1, (DWORD)"Interface\\lo_back_im01.jpg");
+	SetDword(0x004D57E7 + 1, (DWORD)"Interface\\lo_back_im02.jpg");
+	SetDword(0x004D5807 + 1, (DWORD)"Interface\\lo_back_im03.jpg");
+	SetDword(0x004D5827 + 1, (DWORD)"Interface\\lo_back_im04.jpg");
+	SetDword(0x004D5847 + 1, (DWORD)"Interface\\lo_back_im05.jpg");
+	SetDword(0x004D5867 + 1, (DWORD)"Interface\\lo_back_im06.jpg");
 	InitAntiLag();
 	AttachNewEffect();
 SetCompleteHook(0xE9,0x005880F0,0x0058811F); //FIX ASA GM BC
